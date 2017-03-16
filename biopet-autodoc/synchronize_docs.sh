a5f0734d9583126f498650b6d325aae5982c822d
rhpvorderman <R.H.P.Vorderman@LUMC.nl>
2017-03-16 17:33:12 +0100

    Created script to check for undocumented tools. Created jinja2 template to document tools

---
+++ biopet-autodoc/synchronize_docs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#displays the tools not yet in docs
+BIOPET_DIR=~/biopet
+TOOLS_DIR=$BIOPET_DIR/biopet-tools/src/main/scala/nl/lumc/sasc/biopet/tools
+DOCS_DIR=$BIOPET_DIR/docs/tools
+
+echo "Tools not yet in docs: "
+
+for file in $TOOLS_DIR/*.scala
+do
+  file_no_path=${file##*/}
+  file_with_md=${file_no_path%.scala}.md
+  destfile=$DOCS_DIR/$file_with_md
+  if [ ! -f $destfile ]
+    then
+      echo $file_with_md
+  fi
+done
