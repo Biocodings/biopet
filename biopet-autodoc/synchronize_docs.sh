76cbc70906e5edd32fbfe66de351c54937b9213f
rhpvorderman <R.H.P.Vorderman@LUMC.nl>
2017-03-20 17:13:47 +0100

    Created bash script to autogenerate documentation for biopet-tools

--- biopet-autodoc/synchronize_docs.sh
+++ biopet-autodoc/synchronize_docs.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
-#displays the tools not yet in docs
+#Script to document not yet documented tools in biopet
+
+#Declare variables
 BIOPET_DIR=~/biopet
 TOOLS_DIR=$BIOPET_DIR/biopet-tools/src/main/scala/nl/lumc/sasc/biopet/tools
 DOCS_DIR=$BIOPET_DIR/docs/tools
+TEMPLATE=$BIOPET_DIR/biopet-autodoc/tools_doc_template.j2
+BIOPET="java -jar $BIOPET_JAR tool" #Make sure the BIOPET_JAR environment variable is defined!
 
-for file in $TOOLS_DIR/*.scala
+
+for file in $TOOLS_DIR/*.scala #For loop checking all scala files in the tools section
 do
   file_no_path=${file##*/}
-  file_with_md=${file_no_path%.scala}.md
+  file_no_extension=${file_no_path%.scala}
+  file_with_md=${file_no_extension}.md
   destfile=$DOCS_DIR/$file_with_md
-  if [ ! -f $destfile ]
+  if [ ! -f $destfile ] #Checks if (tool_name).md is already present. Existing files are skipped
     then
-      echo $file_with_md
-  fi
+      eval "$BIOPET $file_no_extension -h 2> help.txt"
+      if grep -Fxq "ERROR: command '$file_no_extension' does not exist in module 'tool'" help.txt
+        then
+          echo "Tool $file_no_extension not found"
+        else
+          ./templateparser.py -t "$TEMPLATE" -o "$destfile" -N "$file_no_extension" -L "help.txt"
+          echo "$destfile auto created"
+      
+      fi
+      rm help.txt
+  fi  
 done
--- biopet-autodoc/templateparser.py
+++ biopet-autodoc/templateparser.py
@@ -1,4 +1,6 @@
 #! /usr/bin/env python2
+
+#Script to document new tools using a jinja2 template
 from jinja2 import Environment, FileSystemLoader
 import os
 import argparse
@@ -6,20 +8,33 @@ import ast
 import yaml
 
 if __name__ == "__main__":
-
+    
+    #Block with command line options
     parser=argparse.ArgumentParser(description="")
     parser.add_argument("-t", type=str, nargs=1, help="Templatefile to be used")
     parser.add_argument("-o", type=str, nargs=1, help="output file")
-    parser.add_argument("-d", type=str, nargs=1, help="dictionary containing values")
-    
+    parser.add_argument("-N","--tool-name", type=str, nargs=1, help="name of the tool")
+    parser.add_argument("-O", "--tool-output", type=str, nargs='?', default='')
+    parser.add_argument("-R", "--tool-run", type=str, nargs='?', default='')
+    parser.add_argument("-L", "--tool-option-list", type=str, nargs='?', default= '', help="Text file containing the --help output of the tool")
+
+    #parse arguments
     arguments=parser.parse_args()
-    template=arguments.t
-    output_file=arguments.o
-    dictionary=yaml.load(ast.literal_eval(arguments.d))
-    
-    j2_env = Environment(loader=FileSystemLoader(os.getcwd()))
-    rendered = j2_env.get_template(template).render(tool=dictionary)
-    output_file=open("test.sh", "w")
-    output_file.seek(0)
-    output_file.write(rendered)
-    output_file.close()
+    template=arguments.t[0]
+    output_file=arguments.o[0]
+    tool=dict()
+    tool['name']=arguments.tool_name[0]
+    tool['output']=arguments.tool_output
+    tool['run']=arguments.tool_run
+    help_file=open(arguments.tool_option_list, 'r')
+    tool['option_list']=help_file.read()   #This reads the option list file to a raw string
+
+    # Open jinja2 template and render it with variables
+    j2_env = Environment(loader=FileSystemLoader("/"))
+    rendered = j2_env.get_template(template).render(tool=tool)
+
+    # Write the template results to the output file
+    file=open(output_file, "w")
+    file.seek(0)
+    file.write(rendered)
+    file.close()
