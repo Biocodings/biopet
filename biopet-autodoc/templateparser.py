19f60cb85741a1db0fde25674bdc1c2fb1577ea0
rhpvorderman <R.H.P.Vorderman@LUMC.nl>
2017-03-20 13:57:03 +0100

    Before starting approach using two scripts

--- biopet-autodoc/templateparser.py
+++ biopet-autodoc/templateparser.py
@@ -2,21 +2,23 @@
 from jinja2 import Environment, FileSystemLoader
 import os
 import argparse
+import ast
+import yaml
 
-
-if __name__ == "__main__"
+if __name__ == "__main__":
 
     parser=argparse.ArgumentParser(description="")
-    parser.add_argument("-i", type=str, nargs=1, help="input")
-    parser.add_argument("-o", type=str, nargs=1, help="output")
-    parser.add_argument("-d", type=dict, nargs=1, help="dictionary")
-
+    parser.add_argument("-t", type=str, nargs=1, help="Templatefile to be used")
+    parser.add_argument("-o", type=str, nargs=1, help="output file")
+    parser.add_argument("-d", type=str, nargs=1, help="dictionary containing values")
+    
+    arguments=parser.parse_args()
+    template=arguments.t
+    output_file=arguments.o
+    dictionary=yaml.load(ast.literal_eval(arguments.d))
+    
     j2_env = Environment(loader=FileSystemLoader(os.getcwd()))
-    rendered = j2_env.get_template(
-        "not_yet_documented_tools.j2").render(
-            biopet_dir="~/biopet",
-            tools_dir="floberdy",
-            docs_dir="flopsy")
+    rendered = j2_env.get_template(template).render(tool=dictionary)
     output_file=open("test.sh", "w")
     output_file.seek(0)
     output_file.write(rendered)
